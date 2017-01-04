c9a033992abc1a9a1243a47732fb765182440df8
Vasskob <vasskob@gmail.com>
2017-01-04 22:14:00 +0200

    First commit
    One scene with jumping up bunny

---
+++ Assets/Scripts/BunnyControler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BunnyControler : MonoBehaviour {
+	private Rigidbody2D myRigidbody;
+	public float bunnyJumpForce=500f;
+	// Use this for initialization
+	void Start () {
+		myRigidbody = GetComponent<Rigidbody2D>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonUp ("Jump")) {
+			myRigidbody.AddForce (transform.up * bunnyJumpForce);
+		}
+	}
+}
