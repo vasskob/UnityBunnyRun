b72c79fb1e20f7ae88a83dff4c2389f09f6a03ee
Vasskob <vasskob@gmail.com>
2017-01-05 15:24:01 +0200

    Cactus
    Add many cactuses  and move it left.  Relaunch afrer bunny heat cactus.

--- Assets/Scripts/BunnyControler.cs
+++ Assets/Scripts/BunnyControler.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BunnyControler : MonoBehaviour {
 	private Rigidbody2D myRigidbody;
 	private Animator myAnim;
 	public float bunnyJumpForce=500f;
+	public float bunnyTorque=10f;
 	// Use this for initialization
 	void Start () {
 		myRigidbody = GetComponent<Rigidbody2D>();
@@ -17,9 +19,18 @@ public class BunnyControler : MonoBehaviour {
 	void Update () {
 		if (Input.GetButtonUp ("Jump")) {
 			myRigidbody.AddForce (transform.up * bunnyJumpForce);
+			// myRigidbody.AddForceAtPosition (transform.);
 		}
 	//	myAnim.SetFloat ("wVelocity", Mathf.Abs(myRigidbody.velocity.y));
 		myAnim.SetFloat ("wVelocity", myRigidbody.velocity.y);
+
+	}
+	void OnCollisionEnter2D (Collision2D collision){
+		if (collision.collider.gameObject.layer==LayerMask.NameToLayer("Enemy")){
+
+		//	Application.LoadLevel (Application.loadedLevel);
+			SceneManager.LoadScene ("maneScene");		
+		}
 
 	}
 }
