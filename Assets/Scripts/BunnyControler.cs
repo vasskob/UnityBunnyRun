dbc65fd4b68c5bdde6f0a6383ec35df0aee89470
Vasskob <vasskob@gmail.com>
2017-01-06 18:27:21 +0200

    Paralax
    Adding paralax scroilling

--- Assets/Scripts/BunnyControler.cs
+++ Assets/Scripts/BunnyControler.cs
@@ -12,7 +12,7 @@ public class BunnyControler : MonoBehaviour {
 	private Collider2D myCollider;
 	public Text scoreTxt;
 	private float startTime;
-
+	private int jumpsCount=2;
 	// Use this for initialization
 
 	void Start () {
@@ -26,8 +26,17 @@ public class BunnyControler : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (bunnyHurtTime == -1) {
-			if (Input.GetButtonUp ("Jump")) {
-				myRigidbody.AddForce (transform.up * bunnyJumpForce);
+			if (Input.GetButtonUp ("Jump") && jumpsCount>0) {
+
+				if (myRigidbody.velocity.y < 0) {
+					myRigidbody.velocity = Vector2.zero;
+				}
+				if (jumpsCount == 1) {
+					myRigidbody.AddForce (transform.up * bunnyJumpForce*0.75f);
+				} else {
+					myRigidbody.AddForce (transform.up * bunnyJumpForce);
+				}
+				jumpsCount--;
 				// myRigidbody.AddForceAtPosition (transform.);
 			}
 			//	myAnim.SetFloat ("wVelocity", Mathf.Abs(myRigidbody.velocity.y));
@@ -40,24 +49,24 @@ public class BunnyControler : MonoBehaviour {
 		}
 	}
 	void OnCollisionEnter2D (Collision2D collision){
-		if (collision.collider.gameObject.layer==LayerMask.NameToLayer("Enemy")){
+		if (collision.collider.gameObject.layer == LayerMask.NameToLayer ("Enemy")) {
 
-			foreach (PrefabSpawner spawner in FindObjectsOfType<PrefabSpawner>())
-			{
-			spawner.enabled = false;
+			foreach (PrefabSpawner spawner in FindObjectsOfType<PrefabSpawner>()) {
+				spawner.enabled = false;
 			}
 
-			foreach (MoveLeft moveLefter in FindObjectsOfType<MoveLeft>())
-			{
+			foreach (MoveLeft moveLefter in FindObjectsOfType<MoveLeft>()) {
 				moveLefter.enabled = false;
 			}
 
-			bunnyHurtTime=Time.time;
+			bunnyHurtTime = Time.time;
 			myAnim.SetBool ("bunnyHurt", true);
 			myRigidbody.velocity = Vector2.zero;
 			myRigidbody.AddForce (transform.up * bunnyJumpForce);
 			myCollider.enabled = false;
 
+		} else if (collision.collider.gameObject.layer == LayerMask.NameToLayer ("Ground")) {
+			jumpsCount = 2;
 		}
 
 	}
--- Assets/Scripts/PrefabSpawner.cs
+++ Assets/Scripts/PrefabSpawner.cs
@@ -5,19 +5,30 @@ using UnityEngine;
 public class PrefabSpawner : MonoBehaviour {
 	private float nextSpawn = 0;
 	public Transform prefabToSpawn;
-	public float spawnRate=1;
-	public float randomDellay=1;
+
+	public AnimationCurve spawnCurve;
+	public float curveLengthInSec=30f;
+	private float startTime;
+	private float jitter=0.6f;
+
 
 	// Use this for initialization
 	void Start () {
-		
+		startTime = Time.time;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Time.time > nextSpawn) {
 			Instantiate (prefabToSpawn, transform.position, Quaternion.identity);
-			nextSpawn = Time.time + spawnRate + Random.Range (0, randomDellay);
+			//nextSpawn = Time.time + spawnRate + Random.Range (0, randomDellay);
+			float curvePos=(Time.time - startTime)/curveLengthInSec;
+			if (curvePos > 1f) {
+				curvePos = 1f;
+				startTime = Time.time;
+
+			}
+			nextSpawn = Time.time + spawnCurve.Evaluate (curvePos) + Random.Range(-jitter, jitter);
 		}
 	}
 }
